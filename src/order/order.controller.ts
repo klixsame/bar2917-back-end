60aafb71881184068211eebad45afdf7f0493c1f
kleyfiex <kleyfie92@gmail.com>
2024-06-14 04:46:50 +0300

    +payment

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -3,6 +3,7 @@ import { Auth } from 'src/auth/decorators/auth.decorator';
 import { CurrentUser } from 'src/auth/decorators/user.decorator';
 import { OrderDto } from './order.dto';
 import { OrderService } from './order.service';
+import { PaymentStatusDto } from './payment-status.dto';
 
 @Controller('orders')
 export class OrderController {
@@ -30,9 +31,9 @@ export class OrderController {
     return this.orderService.placeOrder(dto, userId)
   }
 
-  // @HttpCode(200)
-  // @Post('status')
-  // async updateStatus(@Body() dto: PaymentStatusDto) {
-  //   return this.orderService.updateStatus(dto)
-  // }
+  @HttpCode(200)
+  @Post('status')
+  async updateStatus(@Body() dto: PaymentStatusDto) {
+    return this.orderService.updateStatus(dto)
+  }
 }
--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -1,7 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { EnumOrderStatus } from '@prisma/client';
 import { PrismaService } from 'src/prisma.service';
 import { productReturnObject } from 'src/product/return-product.object';
+import * as YooKassa from 'yookassa';
 import { OrderDto } from './order.dto';
+import { PaymentStatusDto } from './payment-status.dto';
+
+const yooKassa = new YooKassa({
+  shopId: process.env['SHOP_ID'],
+  secretKey: process.env['PAYMENT_TOKEN']
+})
 
 @Injectable()
 export class OrderService {
@@ -45,11 +53,18 @@ export class OrderService {
   }
 
   async placeOrder(dto: OrderDto, userId: number) {
+    const deliveryPrice = 100;
+
+    const total = dto.items.reduce((acc, item) => {
+      return acc + item.price * item.quantity;
+    }, 0) + deliveryPrice;
+
     const order = await this.prisma.order.create({
       data: {
         status: dto.status,
         address: dto.address,
         commentary: dto.commentary,
+        total,
         items: {
           create: dto.items
         },
@@ -60,7 +75,47 @@ export class OrderService {
         }
       }
     })
+
+    const payment = await yooKassa.createPayment({
+      amount: {
+        value: total.toFixed(2),
+        currency: 'RUB'
+      },
+      payment_method_data: {
+        type: 'bank_card'
+      },
+      confirmation: {
+        type:'redirect',
+        return_url: 'http://localhost:3000/thanks'
+      },
+      description: `Заказ #${order.id}`
+    })
     
-    return order
+    return payment
 }
+
+  async updateStatus(dto: PaymentStatusDto){
+    if (dto.event === 'payment.waiting_for_capture') {
+      const payment = await yooKassa.capturePayment(dto.object.id) 
+    
+      return payment
+    }
+
+    if (dto.event === 'payment.succeeded') {
+      const orderId = Number(dto.object.description.split('#')[1])
+
+      await this.prisma.order.update({
+        where: {
+          id: orderId
+        },
+        data: {
+          status: EnumOrderStatus.PAYED
+        }
+      })
+
+      return true
+    }
+
+    return true
+  }
 }
